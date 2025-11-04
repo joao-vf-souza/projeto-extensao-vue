a8e478d599c57d631e54614d4787cb927cfe314d
jpalvarezz <joaopedroalvarezsilva@gmail.com>
2025-11-04 14:12:46 -0300

    gabriel polido

--- src/data/tccs.js
+++ src/data/tccs.js
@@ -151,21 +151,21 @@ export const tccs2025 = [
   },
   {
     id: 8,
-    titulo: '',
-    autor: '',
-    orientador: '',
+    titulo: 'ESTUDO SOBRE FRAUDES DIGITAIS E O DESENVOLVIMENTO DE APLICATIVO PARA SMARTPHONES ANDROID E IOS PARA USO EM PALESTRAS DE SENSIBILIZAÇÃO E ESCLARECIMENTO',
+    autor: 'Gabriel Carvalho Polido',
+    orientador: 'Prof. Assoc. Eduardo Martins Morgado',
     curso: 'BCC',
-    descricao: ``,
-    palavrasChave: [''],
-    dataDefesa: '',
+    descricao: `Golpes e fraudes digitais são uma ameaça em constante evolução, principalmente quando utilizam três estratégias de ataque: Phishing, Spoofing e Engenharia Social. Durante a pesquisa, foi destacada a natureza sofisticada dessas ameaças, que visam enganar os usuários e obter acesso a informações confidenciais. O estudo forneceu uma análise aprofundada de cada estratégia, explorando exemplos de casos e técnicas de mitigação. Além disso, foram estudadas as estratégias de ataque que são utilizadas atualmente para que seja possível enfrentar com êxito essas ameaças. tais estratégias de ataque tem o intuito de roubar informações e recursos, visando posteriormente utilizar tais informações para aplicar golpes e fraudes elaboradas na vítima. Assim sendo, foi enfatizada a importância da conscientização e da educação como estratégias cruciais para proteger indivíduos e organizações contra essas ameaças digitais em um ambiente cada vez mais interconectado, dependente da tecnologia e em constante evolução. Foi desenvolvido um aplicativo informático que incentiva as pessoas a tomarem medidas mais proativas para combater eficazmente os golpes e fraudes digitais, garantindo um ambiente cibernético mais seguro.`,
+    palavrasChave: ['Fraudes Digitais', 'Golpes Digitais', 'Phishing', 'Spoofing', 'Engenharia Social', 'Segurança da Informação'],
+    dataDefesa: '2023-11',
     links: [
       {
         label: 'Monografia',
-        url: getTccFileURL('monografia', '')
+        url: getTccFileURL('monografia', 'gabriel_polido_2023.pdf')
       },
       {
         label: 'Apresentação',
-        url: getTccFileURL('apresentacao', '')
+        url: getTccFileURL('apresentacao', 'gabriel_polido_apresentacao_2023.pdf')
       }
     ]
   },
