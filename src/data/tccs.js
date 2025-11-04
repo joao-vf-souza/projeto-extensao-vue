6d619d850957808b36095f99cc6a052713dc7168
jpalvarezz <joaopedroalvarezsilva@gmail.com>
2025-11-04 14:19:57 -0300

    gabriel ticianeli

--- src/data/tccs.js
+++ src/data/tccs.js
@@ -171,21 +171,21 @@ export const tccs2025 = [
   },
   {
     id: 9,
-    titulo: '',
-    autor: '',
-    orientador: '',
+    titulo: 'APLICATIVO GAMIFICADO PARA ORGANIZAÇÃO DE TAREFAS E ROTINAS ACADÊMICAS',
+    autor: 'Gabriel Henrique Garcia Ticianeli',
+    orientador: 'Profª. Me. Juliana da Costa Feitosa',
     curso: 'BCC',
-    descricao: ``,
-    palavrasChave: [''],
-    dataDefesa: '',
+    descricao: `O ingresso na faculdade é um momento de mudanças drásticas no estilo de vida dos estudantes. Ele marca uma transição onde o aluno deve se adaptar a novas responsabilidades que muitas vezes causam uma sobrecarga de trabalho. Esse aumento na demanda pode ser esmagador para aqueles que estão despreparados. Dessa forma, manejar o pouco tempo disponível de maneira efetiva torna-se essencial para o sucesso acadêmico. Como solução para o problema mencionado, o trabalho objetivou desenvolver um aplicativo mobile gamificado para organização de tarefas e rotinas, chamado de “Guilda dos Universitários”. Gamificação é a utilização de elementos de design de jogos em aplicações que não são jogos puros, visando aumentar a interatividade e o engajamento do usuário. Utilizando esta estratégia, o sistema foi projetado para fornecer uma maneira mais envolvente e motivadora de gerenciar a carga de trabalho acadêmica e criar bons hábitos. Por ser o sistema operacional mais usado pelos smartphones, foi escolhido o Android como plataforma para o programa, e a construção do trabalho foi feita na Unity Engine. O aplicativo resultante possui as funções de cadastrar tarefas (tanto únicas quanto repetíveis), contabilizar hábitos diários e formar grupos de usuários para compartilharem dos mesmos afazeres. As ferramentas são complementadas por mecânicas de progressão e recompensas, e os grupos possuem uma tabela de classificação para estimular a competitividade. Os resultados obtidos de uma comparação deste projeto com outros disponíveis no mercado de aplicativos mostra que existem poucas opções para organização de tarefas que aplicam a gamificação, e dentre estes, não há a presença de sistemas cooperativos entre os usuários.`,
+    palavrasChave: ['Aplicativo Gamificado', 'Organização de Tarefas', 'Rotinas Acadêmicas', 'Gamificação', 'Unity'],
+    dataDefesa: '2023-11',
     links: [
       {
         label: 'Monografia',
-        url: getTccFileURL('monografia', '')
+        url: getTccFileURL('monografia', 'gabriel_ticianeli_2023.pdf')
       },
       {
         label: 'Apresentação',
-        url: getTccFileURL('apresentacao', '')
+        url: getTccFileURL('apresentacao', 'gabriel_ticianeli_apresentacao_2023.pdf')
       }
     ]
   }
