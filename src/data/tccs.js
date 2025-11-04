43f461af8b57743994bf6f38e2e40808baaed93f
jpalvarezz <joaopedroalvarezsilva@gmail.com>
2025-11-04 14:53:38 -0300

    geovanna brandao

--- src/data/tccs.js
+++ src/data/tccs.js
@@ -188,6 +188,26 @@ export const tccs2025 = [
         url: getTccFileURL('apresentacao', 'gabriel_ticianeli_apresentacao_2023.pdf')
       }
     ]
+  },
+  {
+    id: 10,
+    titulo: 'QUALIDADE DE SOFTWARE: ANÁLISE DE DESEMPENHO DE FERRAMENTAS DE AUTOMAÇÃO EM TESTES E2E',
+    autor: 'Geovanna Carolina F V T Brandao',
+    orientador: 'Prof. Dr Kleber Rocha de Oliveira',
+    curso: 'BCC',
+    descricao: `O presente trabalho tem como tema a análise de desempenho de ferramentas de automação de testes de ponta-a-ponta. Onde o objetivo é abordar esse estudo de caso a partir do modelo ISSO 25010, aliado aos critérios ortogonais de classificação da abordagem de localização de objetos em tela. Ademais, é realizada uma análise do parecer da indústria de tecnologia em relação a essa categoria de testes. A escolha deste tema é impulsionada pela crescente complexidade dos sistemas de software, a necessidade da redução de custos e aumento da dependência social na tecnologia da informação.`,
+    palavrasChave: ['Qualidade de Software', 'Testes E2E', 'Automação de Testes', 'Desempenho de Ferramentas'],
+    dataDefesa: '2023-11',
+    links: [
+      {
+        label: 'Monografia',
+        url: getTccFileURL('monografia', 'geovanna_brandao_2023.pdf')
+      },
+      {
+        label: 'Apresentação',
+        url: getTccFileURL('apresentacao', 'geovanna_brandao_apresentacao_2023.pdf')
+      }
+    ]
   }
 ];
 
